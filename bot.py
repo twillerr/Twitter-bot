9bdbbb60ca37ad6724e1fb104f12bce3509aea6d
twillerr <willowf2001@gmail.com>
2022-09-13 19:26:20 +0100

    fixed authentication, added func to publish tweet

--- bot.py
+++ bot.py
@@ -1,9 +1,39 @@
 import tweepy
 import config
+import time
+from timeit import default_timer as timer
+from oxpret_scraper import get_recent_post, write_to_file,read_from_file
 
 consumer_key = config.consumer_key
 consumer_secret = config.consumer_secret
 access_token = config.access_token
 access_token_secret = config.access_token_secret
+bearer_token = config.bearer_token
+
+client = tweepy.Client(bearer_token,consumer_key, consumer_secret,access_token, access_token_secret)
+
+
+group_url = "https://www.facebook.com/groups/522129019655282"
+intro_msg = "!! OXPRET UPDATE !!:\n"
+
+def publish_post(msg):
+    client.create_tweet(text=intro_msg+msg, )
+    
+
+def check_and_update():
+    web_post= get_recent_post()
+    local_post= read_from_file()
+    if web_post != local_post:
+        print("New post found!")
+        write_to_file(get_recent_post())
+        publish_post(web_post)
+    else:
+        print("No new posts.")
+
+while True:
+    check_and_update()
+    time.sleep(30)
+    print("checking again...")
+
 
 
