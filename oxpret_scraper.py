b3666dbc4cc80886fdf388d12b9350b1c4802073
twillerr <willowf2001@gmail.com>
2022-09-13 19:21:09 +0100

    Moved generator inside function, added function to read from file

--- oxpret_scraper.py
+++ oxpret_scraper.py
@@ -3,10 +3,8 @@ from facebook_scraper import get_posts
 group_id = "522129019655282"
 cookies_path=r"C:\Users\44747\Twitter-bot\cookies.txt"
 
-#generator for facebook group posts
-postgen = get_posts(group = group_id, pages = 2, cookies = cookies_path)
-
 def get_recent_post():
+    postgen = get_posts(group = group_id, pages = 2, cookies = cookies_path)
     recent_posts=[]
     i=1
     while i<=2:
@@ -18,3 +16,9 @@ def get_recent_post():
 def write_to_file(msg):
     with open("fb_posts.txt", "w") as f:
         f.write(msg)
+
+def read_from_file():
+    with open("fb_posts.txt", "r") as f:
+        text = f.read()
+    return text
+
