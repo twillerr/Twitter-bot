99bd68b9f00b1bb143cdaebbdc9b1d7138a5c244
twillerr <110558807+twillerr@users.noreply.github.com>
2022-09-15 12:36:35 +0100

    Update oxpret_scraper.py
    Added comments for readability.

--- oxpret_scraper.py
+++ oxpret_scraper.py
@@ -4,6 +4,7 @@ group_id = "522129019655282"
 cookies_path=r"C:\Users\44747\Twitter-bot\cookies.txt"
 
 def get_recent_post():
+    #get latest post from generator and add to list
     postgen = get_posts(group = group_id, pages = 2, cookies = cookies_path)
     recent_posts=[]
     i=1
@@ -11,6 +12,7 @@ def get_recent_post():
         recent_posts.append(next(postgen)["text"].encode("ascii","ignore").decode())
         i+=1
     post=recent_posts[1]
+    #first item yielded by generator is pinned post, skips this
     return post
 
 def write_to_file(msg):
